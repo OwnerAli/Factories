19dd9000708883992dd0bccbb81d43d5e531326c
Ali <ogaliwork@gmail.com>
2024-11-05 14:49:19 -0800

    update MarketStockFactory to use new market display items

--- src/main/java/dev/viaduct/factories/guis/menus/markets/MarketStockFactory.java
+++ src/main/java/dev/viaduct/factories/guis/menus/markets/MarketStockFactory.java
@@ -2,13 +2,17 @@ package dev.viaduct.factories.guis.menus.markets;
 
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import dev.viaduct.factories.FactoriesPlugin;
+import dev.viaduct.factories.actions.impl.GiveItemAction;
+import dev.viaduct.factories.conditions.impl.ResourceCondition;
 import dev.viaduct.factories.guis.menus.display_items.MarketDisplayItem;
 import dev.viaduct.factories.guis.menus.gui_items.MarketGuiItem;
 import dev.viaduct.factories.resources.ResourceManager;
 import lombok.Getter;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class MarketStockFactory {
@@ -21,18 +25,15 @@ public class MarketStockFactory {
         resourceManager = FactoriesPlugin.getInstance().getResourceManager();
         //  stock list of potential items.
         possibleItems = new ArrayList<>();
-        possibleItems.add(new MarketDisplayItem("Wood",
-                10.00, Material.OAK_WOOD, new ArrayList<>()));
-        possibleItems.add(new MarketDisplayItem("Stone",
-                12.00, Material.STONE, new ArrayList<>()));
+        possibleItems.add(new MarketDisplayItem("Bundle O' Sticks", new ItemStack(Material.STICK, 20),
+                List.of(new GiveItemAction(new ItemStack(Material.STICK, 20))),
+                new ResourceCondition("wood", 10)));
     }
 
     public static MarketStockFactory getInstance() {
         if (instance == null) {
             synchronized (MarketStockFactory.class) {
-                if (instance == null) {
-                    instance = new MarketStockFactory();
-                }
+                instance = new MarketStockFactory();
             }
         }
         return instance;
@@ -46,4 +47,5 @@ public class MarketStockFactory {
         }
         return marketStock;
     }
+
 }
